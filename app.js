c6f7192b26ad5d8ee7c4ca4ff8ec2a9bd88f7b3e
Aadi2509 <69726355+Aadi2509@users.noreply.github.com>
2022-02-17 21:25:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+//jshint esversion:6
+
+//requiring pakages
+const express= require("express");
+const bodyParser= require("body-parser");
+const ejs= require("ejs");
+
+const app=express();
+
+app.set('view engine','ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+
+
+//listen to the port 3000
+app.listen(3000,function(){
+  console.log("Server is runnig in port 3000");
+});

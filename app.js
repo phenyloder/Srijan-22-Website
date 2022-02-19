48ef182b64109f93e9f53b83fa34c30d45be6ba5
Vinayak Das <vinayakd909@gmail.com>
2022-02-19 21:39:24 +0530

    Add About Section - No Animation

--- app.js
+++ app.js
@@ -2,17 +2,19 @@
 
 //requiring pakages
 const express= require("express");
+const path = require("path");
 const bodyParser= require("body-parser");
 const ejs= require("ejs");
 
 const app=express();
-
 app.set('view engine','ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
+app.get("/",(req,res)=>{
+  res.render("page");
+})
 
 
 //listen to the port 3000

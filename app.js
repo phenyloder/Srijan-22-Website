bf48a9504240c55df008166bc1270cc7cf17971a
doneela <doneeladas@gmail.com>
2022-02-21 18:14:13 +0530

    Add WorkShop Section

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 //jshint esversion:6
 
 //requiring pakages
-const express= require("express");
-const bodyParser= require("body-parser");
-const ejs= require("ejs");
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
-const app=express();
+const app = express();
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
+app.get("/", (req, res) => {
+    res.render("page");
+})
 
 
 //listen to the port 3000
-app.listen(3000,function(){
-  console.log("Server is runnig in port 3000");
+app.listen(3000, function() {
+    console.log("Server is runnig in port 3000");
 });

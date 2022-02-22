73c9bd24fb3789ce505b13942703b091f654bff3
Vinayak Das <vinayakd909@gmail.com>
2022-02-23 01:33:35 +0530

    Add Animation And Transition

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -13,17 +13,27 @@ window.addEventListener("scroll", () => {
     const scrolled = window.scrollY;
     if(scrolled===0)
     {
-        animBox.classList.remove("flipped");
-        eventCircle.classList.remove("flipped");
-        nitsCircle.classList.remove("flipped");
+        nitsCircle.classList.add("transition");
+        setTimeout(()=>{
+            eventCircle.classList.remove("transition");
+            nitsCircle.classList.remove("transition");
+            animBox.classList.remove("flipped");
+            eventCircle.classList.remove("flipped");
+            nitsCircle.classList.remove("flipped");
+        }, 500);
         logo1.classList.remove("hidden");
         logo2.classList.add("hidden");
     }
     else if(Math.ceil(scrolled)===scrollable)
-    {
-        animBox.classList.add("flipped");
-        eventCircle.classList.add("flipped");
-        nitsCircle.classList.add("flipped");
+    { 
+        eventCircle.classList.add("transition");
+        setTimeout(()=>{
+            eventCircle.classList.remove("transition");
+            nitsCircle.classList.remove("transition");
+            animBox.classList.add("flipped");
+            eventCircle.classList.add("flipped");
+            nitsCircle.classList.add("flipped");
+        }, 500);
         logo1.classList.add("hidden");
         logo2.classList.remove("hidden");
     }

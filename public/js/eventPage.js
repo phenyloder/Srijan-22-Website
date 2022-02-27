e8636d9ae679d84bb829b1622e2e393b3a503c9a
JOSU9435 <kamalnetra702@gmail.com>
2022-02-27 18:59:25 +0530

    ADD: event Scroll animation

--- public/js/eventPage.js
+++ public/js/eventPage.js
@@ -18,3 +18,39 @@ dropDown.addEventListener("focusout",() => {
 })
 
 // DROP DOWN
+
+// SCROLL EVENT ANIMATION
+
+const eventList = document.getElementById("eventList");
+
+let eventCounter = 0;
+
+const handleScroll = (e) => {
+    console.log(e.deltaY);
+    
+    const events = [...document.getElementsByClassName("eventContainer")];
+    
+    if(e.deltaY>0){
+        eventCounter = Math.min(events.length-1,eventCounter+1);
+        events.forEach((elem) => {
+            elem.classList.remove("active");
+        })
+        events[eventCounter-1].classList.add("previous");
+        events[eventCounter].classList.remove("next");
+        events[eventCounter].classList.add("active");
+    }else{
+        eventCounter = Math.max(0,eventCounter-1);
+        events.forEach((elem) => {
+            elem.classList.remove("active");
+        });
+        events[eventCounter+1].classList.add("next");
+        events[eventCounter].classList.remove("previous");
+        events[eventCounter].classList.add("active");
+    }
+    
+    eventList.removeEventListener("wheel",handleScroll);
+    setTimeout(() => eventList.addEventListener("wheel",handleScroll), 400);
+}
+eventList.addEventListener(("wheel"),handleScroll);
+
+// SCROLL EVENT ANIMATION

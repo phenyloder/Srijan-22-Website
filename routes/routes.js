b7af2fb3b4fe92d45add0023594706ce0daa1303
doneela <doneeladas@gmail.com>
2022-03-01 01:04:36 +0530

    Add Our Team

--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,13 @@ const express = require("express");
 const { get } = require("express/lib/response");
 const { getIndexPage } = require("../controllers/getIndexPage");
 const { getRegisterPage } = require("../controllers/getRegisterPage");
+const { getOurTeamPage } = require("../controllers/getOurTeamPage");
+
 
 const router = express.Router();
 
 router.get("/", getIndexPage); // serves index page
 router.get("/register", getRegisterPage); //serves register page
+router.get("/our-team", getOurTeamPage); //serves our-team page
 
 module.exports = router;

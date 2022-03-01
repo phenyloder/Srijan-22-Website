ebbb120baa41924b42bcccffd2087c7e2e97d1fb
Vinayak Das <vinayakd909@gmail.com>
2022-03-01 19:35:13 +0530

    Add "Remove Member Feature"

--- public/js/registerForm.js
+++ public/js/registerForm.js
@@ -1,12 +1,36 @@
 const addBtn = document.querySelector(".add-btn");
-const addMemberSec = document.getElementById("add-member");
+const subBtn = document.querySelector(".sub-btn");
+const subMember = document.getElementById("sub-member");
+const members = document.getElementsByClassName("member");
+const memberSec = document.getElementById("members");
 const submitBtn = document.querySelector(".submit");
 const fName = document.getElementsByClassName("fname");
 const lName = document.getElementsByClassName("lname");
 const pNumber = document.getElementsByClassName("pnumber");
 
 addBtn.addEventListener("click", () => {
-    addMemberSec.insertAdjacentHTML("beforebegin", '<br><br><div class="name"><div class="name-f"><label for="fname" class="first-name">First Name*</label><input type="text" id="fname" class="fname" placeholder="John" required></div><div class="name-l"><label for="lname" class="last-name">Last Name*</label><input type="text" id="lname" class="lname" placeholder="DSouza" required></div></div><br><label for="pnumber" class="phone-number">Phone Number*</label><div class="number"><span class="cc">+91</span><input type="tel" id="pnumber" class="pnumber" placeholder="9876543210" required></div><br><label for="gender" class="p-gender">Gender*</label><select name="gender" id="gender"><option value="male">Male</option><option value="female">Female</option></select><br><div class="line" id="line"></div>')
+    // console.log(members);
+    memberSec.insertAdjacentHTML("beforeend", '<div class="member"><br><br><div class="name"><div class="name-f"><label for="fname" class="first-name">First Name*</label><input type="text" id="fname" class="fname" placeholder="John" required></div><div class="name-l"><label for="lname" class="last-name">Last Name*</label><input type="text" id="lname" class="lname" placeholder="DSouza" required></div></div><br><label for="pnumber" class="phone-number">Phone Number*</label><div class="number"><span class="cc">+91</span><input type="tel" id="pnumber" class="pnumber" placeholder="9876543210" required></div><br><label for="gender" class="p-gender">Gender*</label><select name="gender" id="gender"><option value="male">Male</option><option value="female">Female</option></select><br><div class="line" id="line"></div></div>')
+
+    if(members.length>0)
+    {
+        subMember.style.display = "initial";
+    }
+    else{
+        subMember.style.display = "none";
+    }
+});
+
+subBtn.addEventListener("click", () => {
+    memberSec.removeChild(memberSec.lastElementChild);
+
+    if(members.length>0)
+    {
+        subMember.style.display = "initial";
+    }
+    else{
+        subMember.style.display = "none";
+    }
 });
 
 submitBtn.addEventListener("click", () => {
@@ -19,21 +43,22 @@ submitBtn.addEventListener("click", () => {
             i=0;
         }
     }
-    for(let lName of lName)
+    for(let lname of lName)
     {
-        if(lName.value.length===0 )
+        if(lname.value.length===0 )
         {
             i=0;
         }
     }
-    for(let pNumber of pNumber)
+    for(let pnumber of pNumber)
     {
-        if(pNumber.value.length===0 )
+        if(pnumber.value.length===0 )
         {
             i=0;
         }
     }
     if(i===1){
         document.forms[0].submit();
+        alert("Registration Completed Successfully..!!");
     }
 })

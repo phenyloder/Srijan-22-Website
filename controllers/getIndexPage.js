0a0ec0205512be3720efa116e7200997051207f0
Sakshi Garg <sakshigarg0180@gmail.com>
2022-03-08 19:48:17 +0530

    LaunchPage added

--- controllers/getIndexPage.js
+++ controllers/getIndexPage.js
@@ -6,7 +6,12 @@ const getEventPage = (req, res) => {
     res.render('eventPage');
 }
 
+const getLaunchPage = (req , res) => {
+    res.render('launchPage');
+}
+
 module.exports = {
     getIndexPage,
     getEventPage,
+    getLaunchPage,
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { get } = require("express/lib/response");
-const { getIndexPage, getEventPage } = require("../controllers/getIndexPage");
+const { getIndexPage, getEventPage, getLaunchPage } = require("../controllers/getIndexPage");
 
 const router = express.Router();
 
 router.get("/", getIndexPage); // serves index page
 router.get("/events", getEventPage); // serves event page
-
+router.get("/LaunchPage" , getLaunchPage); // serves lauch page
 module.exports = router;

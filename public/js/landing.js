a2702c0a4a11e03d2f990a6119ee4ed67bbf0fb1
Manash Pratim Pathak <mppoff021@gmail.com>
2022-03-14 19:00:59 +0530

    solved anchor tag problem

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,17 +1,20 @@
 document.addEventListener("scroll", scrollChange);
+window.addEventListener("load",function(){
+  document.getElementById("scrollDownButton").href = "#aboutUsSection";
+});
 
 function scrollChange() {
   var x = document.documentElement.scrollTop;
   var y = screen.width;
-  console.log(x);
 
 
   if (y > 500) {
-    if (x < 387) {
+    if (x>=0 && x < 387) {
       document.getElementById("scrollDownButton").innerHTML =
         "SCROLL DOWN <i class='fa-solid fa-arrow-right-long' style='width: 45px'></i>";
       document.getElementById("scrollDownButton").href = "#aboutUsSection";
       document.getElementById("scrollDownButton").style.color = "white";
+      document.getElementById("scrollDownButton").target= "";
 
       document.getElementById("Linkedin").classList.remove("linkedin-1");
       document.getElementById("Facebook").classList.remove("facebook-1");
@@ -25,6 +28,7 @@ function scrollChange() {
         "REGISTER NOW <i class='fa-solid fa-registered' style='width: 45px'></i>";
       document.getElementById("scrollDownButton").href = "/events";
       document.getElementById("scrollDownButton").style.color = "black";
+      document.getElementById("scrollDownButton").target = "_blank"
 
       document.getElementById("Linkedin").classList.add("linkedin-1");
       document.getElementById("Facebook").classList.add("facebook-1");
@@ -160,12 +164,4 @@ function scrollChange() {
   }
 }
 
-document.querySelectorAll('a[href^="#aboutUsSection"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
 
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
-  });
-});

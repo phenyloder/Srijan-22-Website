c645d0eb4f6f2632adecc117e64a8dfffab6575b
Manash Pratim Pathak <mppoff021@gmail.com>
2022-03-14 20:53:51 +0530

    changed the js according to the newly added code

--- public/js/landing.js
+++ public/js/landing.js
@@ -14,6 +14,7 @@ const VerticalLine = document.getElementById("VerticalLine");
 function scrollChange() {
   var x = document.documentElement.scrollTop;
   var y = screen.width;
+  console.log(x);
 
   function InitialState() {
     scrollDownButton.innerHTML =
@@ -62,13 +63,13 @@ function scrollChange() {
   if (y > 500) {
     if (x >= 0 && x < 387) {
       InitialState();
-    } else if (x > 387 && x < 1813) {
+    } else if (x > 387 && x < 2084) {
       changeColorToBlack();
-    } else if (x > 1813 && x < 3189) {
+    } else if (x > 2084 && x < 5308) {
       changeColorToWhite();
-    } else if (x > 3189 && x < 3644) {
+    } else if (x > 5308 && x < 5788) {
       changeColorToBlack();
-    } else if (x > 3644) {
+    } else if (x > 5788) {
       changeColorToWhite();
     }
   }
@@ -76,13 +77,13 @@ function scrollChange() {
   if (y < 500) {
     if (x < 348) {
       InitialState();
-    } else if (x > 348 && x < 1728) {
+    } else if (x > 348 && x < 1926) {
       changeColorToBlack();
-    } else if (x > 1728 && x < 4165) {
+    } else if (x > 1926 && x < 6222) {
       changeColorToWhite();
-    } else if (x > 4165 && x < 4796) {
+    } else if (x > 6222 && x < 6830) {
       changeColorToBlack();
-    } else if (x > 4796) {
+    } else if (x > 6830) {
       changeColorToWhite();
     }
   }

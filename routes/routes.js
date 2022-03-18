b265eddd718ef4b95b173363c522505596f45f88
Gaurav Das <gauravdas014@gmail.com>
2022-03-18 16:56:12 +0530

    fix: typo in import

--- routes/routes.js
+++ routes/routes.js
@@ -7,10 +7,10 @@ const { getRegisterPage } = require("../controllers/getRegisterPage");
 const { getOurTeamPage } = require("../controllers/getOurTeamPage");
 const { getOurDevelopersPage } = require("../controllers/getOurDevelopersPage");
 const { getOurSponsorsPage } = require("../controllers/getOurSponsorsPage");
-const {getPrivacyPolicy} =  require("../controllers/getPrivacyPolicy");
-const {getRefundPolicy} =  require("../controllers/getRefundPolicy");
-const {getTermsConditions} =  require("../controllers/getTermsConditions");
-const User = require("../models/registerInfo")
+const { getPrivacyPolicy } = require("../controllers/getPrivacyPolicy");
+const { getRefundPolicy } = require("../controllers/getRefundPolicy");
+const { getTermsConditions } = require("../controllers/getTermsConditions");
+const User = require("../models/registerinfo");
 
 const router = express.Router();
 
@@ -23,30 +23,29 @@ router.get("/sponsors", getOurSponsorsPage); //serves our-sponsers page
 router.get("/privacyPolicy", getPrivacyPolicy); //serves privacy-policy page
 router.get("/refund-policy", getRefundPolicy); //serves refund-policy page
 router.get("/terms&conditions", getTermsConditions); //serves terms and conditions page
-router.post('/register',async(req,res)=>{
-    //console.log(req.body.fname.length)
-    let team_members=[];
-    //console.log(req.body.fname[0]);
-    for(let i=0;i<req.body.fname.length;i++){
-        let data={
-            fname:req.body.fname[i],
-            lname:req.body.lname[i],
-            cnumber:req.body.cnumber[i],
-            gender:req.body.gender[i],
-        }
-        team_members.push(data);
-    }
+router.post("/register", async (req, res) => {
+  //console.log(req.body.fname.length)
+  let team_members = [];
+  //console.log(req.body.fname[0]);
+  for (let i = 0; i < req.body.fname.length; i++) {
+    let data = {
+      fname: req.body.fname[i],
+      lname: req.body.lname[i],
+      cnumber: req.body.cnumber[i],
+      gender: req.body.gender[i],
+    };
+    team_members.push(data);
+  }
 
-    const user = new User({
-        tname: req.body.tname,
-        members: team_members,
-        // ...req.body
-    })
-    const result = await user.save();
-    // res.send("data saved successfully");
-    res.render('register');
-})
-
+  const user = new User({
+    tname: req.body.tname,
+    members: team_members,
+    // ...req.body
+  });
+  const result = await user.save();
+  // res.send("data saved successfully");
+  res.render("register");
+});
 
 router.get("/LaunchPage", getLaunchPage); // serves lauch page
 module.exports = router;

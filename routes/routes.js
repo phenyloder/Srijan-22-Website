23662d5b6a6de663a23a3c9da5101609c2982590
Aditya <aditya20_ug@cse.nits.ac.in>
2022-03-22 20:50:01 +0530

    pitch please registration

--- routes/routes.js
+++ routes/routes.js
@@ -12,12 +12,14 @@ const { getRefundPolicy } = require("../controllers/getRefundPolicy");
 const { getTermsConditions } = require("../controllers/getTermsConditions");
 const { getEventDetails } = require("../controllers/getEventDetails");
 const User = require("../models/registerinfo");
+const Pitch = require("../models/pitchPlease");
 
 const router = express.Router();
 
 router.get("/", getIndexPage); // serves index page
 router.get("/events", getEventPage); // serves event page
 router.get("/register", getRegisterPage); //serves register page
+router.get("/register/pitch-please", getRegisterPage); //serves register page
 router.get("/our-team", getOurTeamPage); //serves our-team page
 router.get("/our-developers", getOurDevelopersPage); //serves our-team page
 router.get("/sponsors", getOurSponsorsPage); //serves our-sponsers page
@@ -49,6 +51,29 @@ router.post("/register", async (req, res) => {
   const result = await user.save();
   // res.send("data saved successfully");
   res.render("register");
+});
+router.post("/register/pitch-please", async (req, res) => {
+  //console.log(req.body.fname.length)
+  let team_members = [];
+  //console.log(req.body.fname[0]);
+  for (let i = 0; i < req.body.fname.length; i++) {
+    let data = {
+      fname: req.body.fname[i],
+      lname: req.body.lname[i],
+      cnumber: req.body.cnumber[i],
+      gender: req.body.gender[i],
+    };
+    team_members.push(data);
+  }
+
+  const user = new Pitch({
+    tname: req.body.tname,
+    members: team_members,
+    // ...req.body
+  });
+  const result = await user.save();
+  // res.send("data saved successfully");
+  res.render("index");
 });
 
 router.get("/LaunchPage", getLaunchPage); // serves lauch page

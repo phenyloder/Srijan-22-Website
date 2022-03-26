65b1e589cdcc6ca652629887a09a4f4764e689b5
unknown <aadiverma2509@gmail.com>
2022-03-26 14:21:07 +0530

    add json file

---
+++ public/js/event_details_json.js
@@ -0,0 +1,89 @@
+const events_list = [
+    {
+        id: "pitch_please",
+        name: "Pitch Please",
+        is_team_event: true,
+        min_team_members: 1,
+        max_team_members: 4,
+        event_type: "competition",
+        poster_img: "pitchpleases.png",
+        is_registering: true,
+        stages_timeline: [
+            {
+                stage_id: "stage_1",
+                stage_name: "Stage 1 (Online Screening)",
+                stage_start_datetime: "11:59 pm, 10 April 2021",
+                stage_end_datetime: "",
+            },
+            {
+                stage_id: "stage_2",
+                stage_name: "Stage 2 (Pitching)",
+                stage_start_datetime: "11:59 pm, 10 April 2021",
+                stage_end_datetime: "",
+            }
+        ],
+        content: {
+            overview: ["'It's not just about ideas, it's about making the ideas happen.'", "Innovation is the new way of life for most entrepreneurs in an ever more competitive and global economy. Great ideas result in great word of honour paving the way to success. In realization to this vision the fraternity of E-cell NIT Silchar is elated to announce the arrangement of a pitch competition to be held as a part of Srijan 3.0 for the budding entrepreneurs."],
+            outcome: ["Aiming to provide the right stage to showcase the business ideas and models, creating potentiality of early-stage startups help ignite the enthusiastic minds. To evaluate their plan in a risk-free environment by panel of judges would bridge the gap between ideas and reality of an enterprise.", "The competition is open for all. Pitch your plans."],
+            guidelines: ["-> You can participate individually or in a team of a maximum of 4 members.", "-> Please make your pitch-deck submission in this link: <a href=https://forms.gle/h8mLAiQwoX36cuVw8> here </a>", "The judges and the date for the final pitching round will be revealed soon!"],
+            stages: [
+                {
+                    stage_id: "stage_1",
+                    stage_name: "Stage 1 (Online Screening)",
+                    stage_content: ["", ""],
+                    stage_rules: ["Interested teams should submit a PPT about their prospective business plan/idea (in not more than 20 slides).", "The PPT should highlight the following points: Problem, Solution,  Market Size, Market analysis (TAM, SAM, SOM), Competition, traction (if you have any), Business model, Timing (why is it the right time to go ahead with the idea), team background, financial projections (if you have any), product demo (if possible), raise amount. Try to keep the ppt as simple and sleek as possible. Brevity will be appreciated.", "Please find all the resources to creating a good pitch deck here: <a href=https://bit.ly/pitchpleaseresources> here </a>"],
+                },
+                {
+                    stage_id: "stage_2",
+                    stage_name: "Stage 2 (Pitching)",
+                    stage_content: ["", ""],
+                    stage_rules: ["The shortlisted teams will have to pitch their idea in front of an expert investor (or panel).", "Time Limit: 8 mins (6 mins pitch + 2 mins QnA)"],
+                }
+            ]
+        }
+    },
+    {
+        id: "pitch_please",
+        name: "Pitch Please",
+        is_team_event: true,
+        min_team_members: 1,
+        max_team_members: 4,
+        event_type: "competition",
+        poster_img: "pitchpleases.png",
+        is_registering: true,
+        stages_timeline: [
+            {
+                stage_id: "stage_1",
+                stage_name: "Stage 1 (Online Screening)",
+                stage_start_datetime: "11:59 pm, 10 April 2021",
+                stage_end_datetime: "",
+            },
+            {
+                stage_id: "stage_2",
+                stage_name: "Stage 2 (Pitching)",
+                stage_start_datetime: "11:59 pm, 10 April 2021",
+                stage_end_datetime: "",
+            }
+        ],
+        content: {
+            overview: ["'It's not just about ideas, it's about making the ideas happen.'", "Innovation is the new way of life for most entrepreneurs in an ever more competitive and global economy. Great ideas result in great word of honour paving the way to success. In realization to this vision the fraternity of E-cell NIT Silchar is elated to announce the arrangement of a pitch competition to be held as a part of Srijan 3.0 for the budding entrepreneurs."],
+            outcome: ["Aiming to provide the right stage to showcase the business ideas and models, creating potentiality of early-stage startups help ignite the enthusiastic minds. To evaluate their plan in a risk-free environment by panel of judges would bridge the gap between ideas and reality of an enterprise.", "The competition is open for all. Pitch your plans."],
+            guidelines: ["-> You can participate individually or in a team of a maximum of 4 members.", "-> Please make your pitch-deck submission in this link: <a href=https://forms.gle/h8mLAiQwoX36cuVw8> here </a>", "The judges and the date for the final pitching round will be revealed soon!"],
+            stages: [
+                {
+                    stage_id: "stage_1",
+                    stage_name: "Stage 1 (Online Screening)",
+                    stage_content: ["", ""],
+                    stage_rules: ["Interested teams should submit a PPT about their prospective business plan/idea (in not more than 20 slides).", "The PPT should highlight the following points: Problem, Solution,  Market Size, Market analysis (TAM, SAM, SOM), Competition, traction (if you have any), Business model, Timing (why is it the right time to go ahead with the idea), team background, financial projections (if you have any), product demo (if possible), raise amount. Try to keep the ppt as simple and sleek as possible. Brevity will be appreciated.", "Please find all the resources to creating a good pitch deck here: <a href=https://bit.ly/pitchpleaseresources> here </a>"],
+                },
+                {
+                    stage_id: "stage_2",
+                    stage_name: "Stage 2 (Pitching)",
+                    stage_content: ["", ""],
+                    stage_rules: ["The shortlisted teams will have to pitch their idea in front of an expert investor (or panel).", "Time Limit: 8 mins (6 mins pitch + 2 mins QnA)"],
+                }
+            ]
+        }
+    }
+]
+

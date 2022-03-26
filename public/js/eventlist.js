cb2c5f054c4393c7ba2874a2bfc62607bb8cab27
Adrita <missadritaburagohain@gmail.com>
2022-03-26 18:04:37 +0530

    updated

--- public/js/eventlist.js
+++ public/js/eventlist.js
@@ -4,7 +4,7 @@ let eventIcon = document.getElementById("event-icon")
 var query1 = window.matchMedia("(max-width:730px)");
 var query2 = window.matchMedia("(max-width:500px)");
 window.addEventListener('scroll',(e)=>{
-   
+   console.log("heu")
     let eventList = document.getElementById('event-scroll-container');
 //   var x = eventList.scrollLeft;
   let x = window.scrollY;
@@ -49,38 +49,38 @@ window.addEventListener('scroll',(e)=>{
 // }
 if(query2.matches){
     if(x<2890.6){
-        eventLogo.src='images/technerd.svg'
+        eventLogo.src='images/pitch.png'
     }
-    if(x>=2890.6 && x<3176.6){
-        eventLogo.src='images/en auction.svg'
+    if(x>=2890.6){
+        eventLogo.src='images/technerd.png'
     }
-    if(x>=3176.6 && x<3523.3){
-        eventLogo.src='images/pitch.svg'
-    }
-    if(x>=3523.3 && x<3816){
-        eventLogo.src='images/pannel discussion.svg'
-    }
-    if(x>=3816){
-        eventLogo.src='images/idea exchange.svg'
-    }
+    // if(x>=3176.6 && x<3523.3){
+    //     eventLogo.src='images/pitch.svg'
+    // }
+    // if(x>=3523.3 && x<3816){
+    //     eventLogo.src='images/pannel discussion.svg'
+    // }
+    // if(x>=3816){
+    //     eventLogo.src='images/idea exchange.svg'
+    // }
 }
 else{
    
     if(x<2777){
-        eventLogo.src='images/technerd.png'
-    }
-    if(x>=2777 && x<3152.6){
-        eventLogo.src='images/en auction.png'
-    }
-    if(x>=3152.6 && x<3513.3){
         eventLogo.src='images/pitch.png'
     }
-    if(x>=3513.3 && x<3882.6){
-        eventLogo.src='images/pannel discussion.png'
+    if(x>=2777){
+        eventLogo.src='images/technerd.png'
     }
-    if(x>=3882.6){
-        eventLogo.src='images/idea exchange.png'
-    }
+    // if(x>=3152.6 && x<3513.3){
+    //     eventLogo.src='images/pitch.png'
+    // }
+    // if(x>=3513.3 && x<3882.6){
+    //     eventLogo.src='images/pannel discussion.png'
+    // }
+    // if(x>=3882.6){
+    //     eventLogo.src='images/idea exchange.png'
+    // }
   
 }
 
